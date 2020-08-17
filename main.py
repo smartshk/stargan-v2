a56e00b9e3a60aa157bbce2e3e7b01828270d723
Votre Nom <Vous@exemple.com>
2020-08-17 10:54:58 +0200

    exp 002

--- main.py
+++ main.py
@@ -84,10 +84,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # model arguments
-    parser.add_argument('--img_size', type=int, default=256,
-                        help='Image resolution')
-    parser.add_argument('--num_domains', type=int, default=2,
-                        help='Number of domains')
+    # parser.add_argument('--img_size', type=int, default=256,
+    #                     help='Image resolution')
+    # parser.add_argument('--num_domains', type=int, default=2,
+    #                     help='Number of domains')
     parser.add_argument('--latent_dim', type=int, default=16,
                         help='Latent vector dimension')
     parser.add_argument('--hidden_dim', type=int, default=512,
@@ -102,22 +102,22 @@ if __name__ == '__main__':
                         help='Weight for cyclic consistency loss')
     parser.add_argument('--lambda_sty', type=float, default=1,
                         help='Weight for style reconstruction loss')
-    parser.add_argument('--lambda_ds', type=float, default=1,
-                        help='Weight for diversity sensitive loss')
+    # parser.add_argument('--lambda_ds', type=float, default=1,
+    #                     help='Weight for diversity sensitive loss')
     parser.add_argument('--ds_iter', type=int, default=100000,
                         help='Number of iterations to optimize diversity sensitive loss')
-    parser.add_argument('--w_hpf', type=float, default=1,
-                        help='weight for high-pass filtering')
+    # parser.add_argument('--w_hpf', type=float, default=1,
+    #                     help='weight for high-pass filtering')
 
     # training arguments
     parser.add_argument('--randcrop_prob', type=float, default=0.5,
                         help='Probabilty of using random-resized cropping')
     parser.add_argument('--total_iters', type=int, default=100000,
                         help='Number of total iterations')
-    parser.add_argument('--resume_iter', type=int, default=0,
-                        help='Iterations to resume training/testing')
-    parser.add_argument('--batch_size', type=int, default=8,
-                        help='Batch size for training')
+    # parser.add_argument('--resume_iter', type=int, default=0,
+    #                     help='Iterations to resume training/testing')
+    # parser.add_argument('--batch_size', type=int, default=8,
+    #                     help='Batch size for training')
     parser.add_argument('--val_batch_size', type=int, default=32,
                         help='Batch size for validation')
     parser.add_argument('--lr', type=float, default=1e-4,
@@ -134,49 +134,81 @@ if __name__ == '__main__':
                         help='Number of generated images per domain during sampling')
 
     # misc
-    parser.add_argument('--mode', type=str, required=True,
-                        choices=['train', 'sample', 'eval', 'align'],
-                        help='This argument is used in solver')
+    # parser.add_argument('--mode', type=str, required=True,
+    #                     choices=['train', 'sample', 'eval', 'align'],
+    #                     help='This argument is used in solver')
     parser.add_argument('--num_workers', type=int, default=4,
                         help='Number of workers used in DataLoader')
     parser.add_argument('--seed', type=int, default=777,
                         help='Seed for random number generator')
 
     # directory for training
-    parser.add_argument('--train_img_dir', type=str, default='data/celeba_hq/train',
-                        help='Directory containing training images')
-    parser.add_argument('--val_img_dir', type=str, default='data/celeba_hq/val',
-                        help='Directory containing validation images')
-    parser.add_argument('--sample_dir', type=str, default='expr/samples',
-                        help='Directory for saving generated images')
-    parser.add_argument('--checkpoint_dir', type=str, default='expr/checkpoints',
-                        help='Directory for saving network checkpoints')
+    # parser.add_argument('--train_img_dir', type=str, default='data/celeba_hq/train',
+    #                     help='Directory containing training images')
+    # parser.add_argument('--val_img_dir', type=str, default='data/celeba_hq/val',
+    #                     help='Directory containing validation images')
+    # parser.add_argument('--sample_dir', type=str, default='expr/samples',
+    #                     help='Directory for saving generated images')
+    # parser.add_argument('--checkpoint_dir', type=str, default='expr/checkpoints',
+    #                     help='Directory for saving network checkpoints')
 
     # directory for calculating metrics
-    parser.add_argument('--eval_dir', type=str, default='expr/eval',
-                        help='Directory for saving metrics, i.e., FID and LPIPS')
+    # parser.add_argument('--eval_dir', type=str, default='expr/eval',
+    #                     help='Directory for saving metrics, i.e., FID and LPIPS')
 
     # directory for testing
-    parser.add_argument('--result_dir', type=str, default='expr/results',
-                        help='Directory for saving generated images and videos')
-    parser.add_argument('--src_dir', type=str, default='assets/representative/celeba_hq/src',
-                        help='Directory containing input source images')
-    parser.add_argument('--ref_dir', type=str, default='assets/representative/celeba_hq/ref',
-                        help='Directory containing input reference images')
-    parser.add_argument('--inp_dir', type=str, default='assets/representative/custom/female',
-                        help='input directory when aligning faces')
-    parser.add_argument('--out_dir', type=str, default='assets/representative/celeba_hq/src/female',
-                        help='output directory when aligning faces')
+    # parser.add_argument('--result_dir', type=str, default='expr/results',
+    #                     help='Directory for saving generated images and videos')
+    # parser.add_argument('--src_dir', type=str, default='assets/representative/celeba_hq/src',
+    #                     help='Directory containing input source images')
+    # parser.add_argument('--ref_dir', type=str, default='assets/representative/celeba_hq/ref',
+    #                     help='Directory containing input reference images')
+    # parser.add_argument('--inp_dir', type=str, default='assets/representative/custom/female',
+    #                     help='input directory when aligning faces')
+    # parser.add_argument('--out_dir', type=str, default='assets/representative/celeba_hq/src/female',
+    #                     help='output directory when aligning faces')
 
     # face alignment
-    parser.add_argument('--wing_path', type=str, default='expr/checkpoints/wing.ckpt')
-    parser.add_argument('--lm_path', type=str, default='expr/checkpoints/celeba_lm_mean.npz')
+    # parser.add_argument('--wing_path', type=str, default='expr/checkpoints/wing.ckpt')
+    # parser.add_argument('--lm_path', type=str, default='expr/checkpoints/celeba_lm_mean.npz')
 
     # step size
     parser.add_argument('--print_every', type=int, default=10)
     parser.add_argument('--sample_every', type=int, default=5000)
     parser.add_argument('--save_every', type=int, default=10000)
     parser.add_argument('--eval_every', type=int, default=50000)
+
+    ### 002 Without to Aligner
+    # parser.add_argument('--mode', type=str, default='train')
+    # parser.add_argument('--num_domains', type=int, default=2)
+    # parser.add_argument('--resume_iter', type=int, default=0')
+    # parser.add_argument('--w_hpf', type=float, default=0)
+    # parser.add_argument('--lambda_ds', type=float, default=1)
+    # parser.add_argument('--train_img_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Data/W2A/train')
+    # parser.add_argument('--val_img_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Data/W2A/valid')
+    # parser.add_argument('--sample_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/samples')
+    # parser.add_argument('--checkpoint_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/checkpoints')
+    # parser.add_argument('--eval_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/eval')
+    # parser.add_argument('--batch_size', type=int, default=4)
+    # parser.add_argument('--img_size', type=int, default=256)
+
+    ### 002 Sampling
+    parser.add_argument('--mode', type=str, default='sample')
+    parser.add_argument('--num_domains', type=int, default=2)
+    parser.add_argument('--resume_iter', type=int, default=100000)
+    parser.add_argument('--w_hpf', type=float, default=0)
+    parser.add_argument('--result_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/results')
+    parser.add_argument('--src_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/representative/src')
+    parser.add_argument('--ref_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/representative/ref')
+
+    parser.add_argument('--lambda_ds', type=float, default=1)
+    parser.add_argument('--train_img_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Data/W2A/train')
+    parser.add_argument('--val_img_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Data/W2A/valid')
+    parser.add_argument('--sample_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/samples')
+    parser.add_argument('--checkpoint_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/checkpoints')
+    parser.add_argument('--eval_dir', type=str, default='/opt/deeplearning/tai/StarGanV2/Models/002_w2a/eval')
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--img_size', type=int, default=256)
 
     args = parser.parse_args()
     main(args)
